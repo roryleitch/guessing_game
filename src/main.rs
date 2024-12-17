06ffefbb64fa7d2c8ce4770bed5c0a40155d005a
Rory Leitch <rory@leitches.net>
2024-12-17 18:16:23 +0000

    Stage Three

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
-
 fn main() {
     println!("Guess the number!");
 
@@ -16,5 +16,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less  => println!("Too Small!"),
+        Ordering::Greater => println!("Too Big!"),
+        Ordering::Equal => println!("You Win!"),
+    }
 }
